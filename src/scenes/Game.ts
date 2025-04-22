2acb616d83925fde33c36731b6fd701b20fb24d3
Vikram Subramanian <vikram.subramanian@gmail.com>
2025-04-22 16:16:09 +0200

    Make objects into sprites with proper depth

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -26,6 +26,7 @@ export class Game extends Scene {
     // Add colliders from the tilemap layers
     ground?.setCollisionFromCollisionGroup();
     objects?.setCollisionFromCollisionGroup();
+    objects.setVisible(false);
 
     // Handle keyboard input.
     this.cursors = this.input.keyboard!.createCursorKeys();
@@ -43,15 +44,54 @@ export class Game extends Scene {
     // Setup collision between player and tilemap layers
     this.physics.add.collider(this.player, ground!);
     this.physics.add.collider(this.player, objects!);
+
+    // Get all tile indices which are marked as objects.
+    const objectTiles = new Map<number, number[]>();
+    for (
+      let i = tileset?.firstgid!;
+      i <= tileset?.firstgid! + tileset?.total!;
+      i++
+    ) {
+      const props = tileset?.getTileProperties(i) as {
+        object: boolean;
+        origin: number;
+      };
+      if (props && props['object']) {
+        const origin = props['origin'] || 0;
+        const val = objectTiles.get(origin) || [];
+        val.push(i);
+        objectTiles.set(origin, val);
+      }
+    }
+
+    // Replace the object tiles with sprites...
+    for (const origin of objectTiles.keys()) {
+      const sprites = map.createFromTiles(
+        objectTiles.get(origin)!,
+        undefined,
+        {
+          useSpriteSheet: true,
+        },
+        this,
+        this.camera,
+        'objects',
+      );
+      for (const s of sprites!) {
+        s.setDepth(s.getWorldPoint().y + s.height / 2 + s.height * origin);
+      }
+    }
   }
 
   update() {
+    this.player.setDepth(
+      this.player.getWorldPoint().y + this.player.height / 2,
+    );
     this.player.setVelocity(0);
 
     const hor = this.cursors.left.isDown || this.cursors.right.isDown;
     const ver = this.cursors.up.isDown || this.cursors.down.isDown;
 
-    // Adjust scale if moving horizontally so that magnitude is maintained.
+    // Adjust scale if moving diagonally so that magnitude is maintained.
     let scale = 1.0;
     if (hor && ver) {
       scale = 1.0 / Math.sqrt(2);
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -29,7 +29,10 @@ export class Preloader extends Scene {
     this.load.image('logo', 'logo.png');
 
     this.load.tilemapTiledJSON('map', 'maps/forest.tmj');
-    this.load.image('forest', 'tilesets/forest/gentle forest v01.png');
+    this.load.spritesheet('forest', 'tilesets/forest/gentle forest v01.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
 
     this.load.spritesheet('king', 'characters/npc/npc king A v03.png', {
       frameWidth: 32,
