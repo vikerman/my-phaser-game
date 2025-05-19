2d3a661cbd667add1519b23c28cb87525524269d
Vikram Subramanian <vikram.subramanian@gmail.com>
2025-05-19 22:51:03 +0200

    add FPS counter and set maxTextures to 2 to workaround FPS issues on Macbook Air

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,18 @@ import { Glow2 } from './scenes/Glow2';
 
 import { Game, Types } from 'phaser';
 
+import Stats from 'stats.js';
+
+const stats1 = new Stats();
+stats1.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats1.dom.style.cssText = 'position:absolute;top:0px;left:0px;';
+document.body.appendChild(stats1.dom);
+
+const stats2 = new Stats();
+stats2.showPanel(2); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats2.dom.style.cssText = 'position:absolute;top:0px;left:80px;';
+document.body.appendChild(stats2.dom);
+
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
@@ -15,6 +27,7 @@ const config: Types.Core.GameConfig = {
   width: 1280,
   height: 800,
   parent: 'game-container',
+  maxTextures: 2,
   scale: {
     mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
@@ -32,4 +45,16 @@ const config: Types.Core.GameConfig = {
   scene: [Boot, Preloader, Glow, Glow2, /* MainMenu, */ MainGame, GameOver],
 };
 window.devicePixelRatio = 1;
-export default new Game(config);
+const game = new Game(config);
+
+game.events.on(Phaser.Core.Events.PRE_STEP, () => {
+  stats1.begin();
+  stats2.begin();
+});
+
+game.events.on(Phaser.Core.Events.POST_RENDER, () => {
+  stats1.end();
+  stats2.end();
+});
+
+export default game;
